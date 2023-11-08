0f9e214456399455dc2993e6f07f1c213c1eb0a8
Luis Masuelli <luis@datagran.io>
2023-11-07 20:54:34 -0500

    Adding the currency definition plug-in.

---
+++ events-grabber/app/handlers/currency_definition_plugin.py
@@ -0,0 +1,32 @@
+from .base import MetaverseRelatedContractEventHandler
+
+
+class CurrencyDefinitionPluginContractHandler(MetaverseRelatedContractEventHandler):
+    """
+    This handler stands for the CurrencyDefinitionPlugin contract.
+    """
+
+    def get_event_names(self):
+        """
+        Returns the list of events: for parameter change, metadata
+          update, and first definition.
+        :return: The list of supported events.
+        """
+
+        return ["CurrencyDefinitionCostUpdated", "CurrencyDefined", "CurrencyMetadataUpdated"]
+
+    def process_event(self, event: dict):
+        """
+        Processes one of the events: definition, update or definition-cost update.
+        :param event: The event to process.
+        """
+
+        event_name = event['event']
+        args = event['args']
+
+        if event_name == "CurrencyDefinitionCostUpdated":
+            self._set_parameter("currency_definition_cost", self._get_arg(args, "newCost"))
+        elif event_name == "CurrencyDefined":
+            self._download_metadata(self._get_arg(args, "tokenId"), "currency")
+        elif event_name == "CurrencyMetadataUpdated":
+            self._download_metadata(self._get_arg(args, "tokenId"), "currency")
