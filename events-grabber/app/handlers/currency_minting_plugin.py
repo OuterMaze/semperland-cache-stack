b5c51cbe6bbdb44fb347541a7e0c8adb0d77a3e7
Luis Masuelli <luis@datagran.io>
2023-11-07 20:59:45 -0500

    Removing old code and adding the CurrencyMintingPlugin.

---
+++ events-grabber/app/handlers/currency_minting_plugin.py
@@ -0,0 +1,29 @@
+from .base import MetaverseRelatedContractEventHandler
+
+
+class CurrencyMintingPluginContractHandler(MetaverseRelatedContractEventHandler):
+    """
+    This handler stands for the CurrencyMintingPlugin contract.
+    """
+
+    def get_event_names(self):
+        """
+        Returns the list of events for parameter change.
+        :return: The list of supported events.
+        """
+
+        return ["CurrencyMintCostUpdated", "CurrencyMintAmountUpdated"]
+
+    def process_event(self, event: dict):
+        """
+        Processes the cost/amount update event.
+        :param event: The event being processed.
+        """
+
+        event_name = event['event']
+        args = event['args']
+
+        if event_name == "CurrencyMintCostUpdated":
+            self._set_parameter("currency_minting_cost", self._get_arg(args, "newCost"))
+        elif event_name == "CurrencyMintAmountUpdated":
+            self._set_parameter("currency_minting_amount", self._get_arg(args, "newAmount"))
