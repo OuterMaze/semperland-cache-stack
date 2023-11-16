c6ffff1ffb2129bf8c59c31663ce9eb3a228c111
Luis Masuelli <luis@datagran.io>
2023-11-16 12:22:33 -0500

    Adding some replica settings.

--- events-grabber/development/mongo-entrypoint.sh
+++ events-grabber/development/mongo-entrypoint.sh
@@ -1,3 +1,4 @@
-cp /etc/mongo/keyfile-source /etc/mongo/keyfile
+mkdir -p /etc/mongo
+cp /tmp/mongo/keyfile-source /etc/mongo/keyfile
 chmod 0400 /etc/mongo/keyfile
-mongod --replSet rs0 --keyFile "/etc/mongo/keyfile"
+mongod --replSet rs0 --keyFile "/etc/mongo/keyfile" --bind_ip_all
