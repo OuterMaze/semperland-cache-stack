d5930dddbdcc303285b7a06492e3bad130a4276e
Luis Masuelli <luis@datagran.io>
2023-11-17 22:27:20 -0500

    Restoring the DB and mating everything non-transactional.

--- events-grabber/app/main.py
+++ events-grabber/app/main.py
@@ -6,18 +6,20 @@ from web3 import Web3, HTTPProvider
 from runner import run_all
 
 
-def main(mongodb_server_url: str, db_name: str, gateway_url: str, metaverse_contract_address: str):
+def main(mongodb_server_url: str, db_name: str, gateway_url: str, use_transactions: bool,
+         metaverse_contract_address: str):
     """
     The full SemperLand events grabber.
     :param mongodb_server_url: The URL of the MongoDB server.
     :param db_name: The database name
     :param gateway_url: The URL of the EVM Gateway to use.
+    :param use_transactions: Whether to use transactions or not.
     :param metaverse_contract_address: The address of the metaverse contract.
     """
 
     with ILock("semperland.cache"):
         client = MongoClient(mongodb_server_url)
-        run_all(client, db_name, Web3(HTTPProvider(gateway_url)), metaverse_contract_address)
+        run_all(client, db_name, Web3(HTTPProvider(gateway_url)), use_transactions, metaverse_contract_address)
 
 
 if __name__ == "__main__":
@@ -30,5 +32,5 @@ if __name__ == "__main__":
             os.getenv("MONGODB_PORT", "27017")
         )
 
-    main(server_url, os.environ["DB_NAME"], os.environ["GATEWAY_URL"],
+    main(server_url, os.environ["DB_NAME"], os.environ["GATEWAY_URL"], os.getenv('MONGODB_TRANSACTIONS') == 'yes',
          os.environ["METAVERSE_CONTRACT_ADDRESS"])
